9223663f698ff790de3fdaa517f491665fd4b980
Mateusz <mateusz.guzek@protonmail.com>
2024-04-29 19:59:41 +0200

    added todo
    Signed-off-by: Mateusz <mateusz.guzek@protonmail.com>

--- app/src/main/java/pl/mateusz/blinker/modules/storage/Account.kt
+++ app/src/main/java/pl/mateusz/blinker/modules/storage/Account.kt
@@ -3,15 +3,13 @@ package pl.mateusz.blinker.modules.storage
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-// i should
+// TODO use shared preferences to keep track of the main account that is used to show all data in app
 @Entity(tableName = "accounts")
 data class Account(var email: String, var apiToken: String) {
 
     @PrimaryKey(autoGenerate = true)
     var uid: Int = 0
 
-
-    var isMain: Boolean = false
 
     var name: String = ""
 
